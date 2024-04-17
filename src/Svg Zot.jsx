f8ffa1fbd8610f92e4cbd73d55998eae57da0c74
Arkady Elterman <elterman@gmail.com>
2024-04-16 21:08:37 -0700

    .

--- src/Svg Zot.jsx
+++ src/Svg Zot.jsx
@@ -4,7 +4,7 @@ const SvgZot = ({ style = {}, width, color = 'white', done = null, viewBox = '0 
   const opacity = done ? 0.35 : 0.05;
 
   return (
-    <motion.svg id="svg-x" style={{ ...style, display: 'block' }} width={width} height={width} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg"
+    <motion.svg id="svg-zot" style={{ ...style, display: 'block' }} width={width} height={width} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg"
       animate={{ opacity }} transition={{ duration: 1 }}>
       <polygon fill={color} strokeLinejoin="round" points="64,0 99,0 47,46 71,47 1,99 0,99 35,60 13,60" />
     </motion.svg>
