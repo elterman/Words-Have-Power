acedf1c8907efef2e9098cfecb9dee5d16562de0
Arkady Elterman <elterman@gmail.com>
2024-04-17 15:36:59 -0700

    .

--- src/Help.jsx
+++ src/Help.jsx
@@ -12,7 +12,7 @@ const Help = () => {
         'The game begins with a randomly chosen 4-letter word.',
         <div className='help-ob'>Example:</div>,
         <img className='help-ob' style={{ margin: '-10px 0 5px 0' }} src={Example} alt='example' width={360} />,
-        `The game ends when a player's battery reaches a set charge (${P2W_DEFAULT} units by default). If that player started the game, the other player takes one more turn.`,
+        `The game ends when a player's battery reaches a set charge (${P2W_DEFAULT} units by default). If that player started the game, the opponent takes one more turn.`,
         'The player with the higher battery charge wins.',
         'Optional: a word cannot start with the same sequence of up to 3 letters as the previous word.',
         'Optional: up to 3 randomly chosen letters on the keyboard can be "blacklisted" (made unplayable) on each turn.',
